3a0496ad0d2b39b8406500167f860d2195d50060
sru_kinj1995 <srujankinjawadekar1@gmail.com>
2023-09-16 10:04:45 +0530

    mongodb connection

--- Backend/Models/UserModel.jsx
+++ Backend/Models/UserModel.jsx
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs')
 
 const userschema = mongoose.Schema(
 {
     name: { type: String, trim: true, required: true },
-    email: { type: String, trim: true, required: true },
+    email: { type: String, trim: true, required: true , unique:true },
     password: { type: String, trim: true, required: true },
     pic: {
         type: String,
-        trim: true,
+        trim:true,
         default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
@@ -18,5 +19,18 @@ const userschema = mongoose.Schema(
 
 );
 
+userschema.methods.matchPassword = async function(reqpassword){
+  return await bcrypt.compare(reqpassword, this.password);
+}
+
+userschema.pre('save', async function (next){
+  if (!this.isModified){
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(15);
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 const UserSchema = mongoose.model("UserSchema", userschema)
-export default UserSchema;
+module.exports= UserSchema;
--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,19 @@
-const {chats} = require('./data.js')
-
 const express = require("express")
 const dotenv = require("dotenv");
-const connect = require('./Database/Config/Db.js');
+const connect = require('./Database/Db.js');
+const router = require('./Routes/Routes.js');
 const app = express();
+const {notfound, errorhandler} = require('./Middleware/ErrorMiddleware.js')
 dotenv.config();
 connect();
+
+
 const port= process.env.PORT
+app.use(express.json());
+
+app.use('/api/user', router)
+
+app.use(notfound);
+app.use(errorhandler)
+
 app.listen(3030,console.log( `server is up .. at ${port}`))
-
-app.get("/", (req,res)=>{
-    res.send(`/Backend/Data/Data.js`)
-})
-
-app.get("/api/chats", (req,res)=>{
-    res.send(chats) 
-})
-
-app.get('/chats', (req, res)=>{
-    res.send("the server is running for chats")
-})
-
